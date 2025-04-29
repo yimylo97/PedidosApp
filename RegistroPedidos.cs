1ebe252168df09c5fe763db8497654fdaf012395
Yimy Herrera <yimyherrera97@gmail.com>
2025-04-29 14:48:22 -0500

    Aplicar Singleton para el registro de pedidos

---
+++ RegistroPedidos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public sealed class RegistroPedidos
+    {
+        private static RegistroPedidos _instancia;
+        private static readonly object _lock = new object();
+        public List<Pedido> Pedidos { get; private set; }
+        private RegistroPedidos() => Pedidos = new List<Pedido>();
+        public static RegistroPedidos Instancia
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _instancia ??= new RegistroPedidos();
+                }
+            }
+        }
+        public void AgregarPedido(Pedido pedido)
+        {
+            Pedidos.Add(pedido);
+        }
+    }
+}
