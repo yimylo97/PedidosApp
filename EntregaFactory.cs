a131ab928a6a0a7be01965a0d8ad8ab180c7b52b
Yimy Herrera <yimyherrera97@gmail.com>
2025-04-29 14:46:28 -0500

    Implementar el Factory Method

---
+++ EntregaFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public static class EntregaFactory
+    {
+        public static IMetodoEntrega CrearEntrega(string tipoProducto, bool urgente, double
+        peso)
+        {
+            if (tipoProducto == "tecnología" && urgente)
+                return new EntregaDron();
+            else if (tipoProducto == "accesorio")
+                return new EntregaMoto();
+            else if (tipoProducto == "componente" || peso > 10)
+                return new EntregaCamion();
+            else
+                return new EntregaMoto();
+        }
+    }
+}
