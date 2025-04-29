968bc47ebf707286de388c1305cc586f0f24b30c
Yimy Herrera <yimyherrera97@gmail.com>
2025-04-29 14:47:35 -0500

    Crear la Clase Pedido

---
+++ Pedido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public class Pedido
+    {
+        public string Cliente { get; set; }
+        public string Producto { get; set; }
+        public bool Urgente { get; set; }
+        public double Peso { get; set; }
+        public int Distancia { get; set; }
+        public IMetodoEntrega MetodoEntrega { get; set; }
+        public Pedido(string cliente, string producto, bool urgente, double peso, int distancia)
+        {
+            Cliente = cliente;
+            Producto = producto;
+            Urgente = urgente;
+            Peso = peso;
+            Distancia = distancia;
+            MetodoEntrega = EntregaFactory.CrearEntrega(producto, urgente, peso);
+        }
+        public double ObtenerCosto() => MetodoEntrega.CalcularCosto(Distancia);
+    }
+}
