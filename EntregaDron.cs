ac162fa68e9a52b94d1bdb833c4e4c6aae08f49f
Yimy Herrera <yimyherrera97@gmail.com>
2025-04-29 14:41:15 -0500

    Implementar las Estrategias de Entrega

---
+++ EntregaDron.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp
+{
+    public class EntregaDron : IMetodoEntrega
+    {
+        public double CalcularCosto(int km) => 20 * km;
+        public string TipoEntrega() => "Dron";
+    }
+    public class EntregaMoto : IMetodoEntrega
+    {
+        public double CalcularCosto(int km) => 10 * km;
+        public string TipoEntrega() => "Motocicleta";
+    }
+    public class EntregaCamion : IMetodoEntrega
+    {
+        public double CalcularCosto(int km) => 5 * km;
+        public string TipoEntrega() => "Camión";
+    }
+}
